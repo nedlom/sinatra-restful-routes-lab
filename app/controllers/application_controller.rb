6b2d701291602e45a8d4b2511a4998529d72aec4
Nick <nelom3@gmail.com>
2021-07-01 13:59:44 -0700

    commit tree 7883ef7507cf6b6a86e84160f85fda810e6f9b4e

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,44 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/' do 
+    redirect '/recipes'
+  end
+
+  get '/recipes' do
+    @recipes = Recipe.all 
+    erb :index
+  end
+
+  get '/recipes/new' do 
+    erb :new
+  end
+
+  post '/recipes' do 
+    @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+  get '/recipes/:id' do 
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+   
+    @recipe.update(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+  delete '/recipes/:id' do 
+    Recipe.find(params[:id]).delete
+    redirect "/recipes"
+  end
 
 end
---
+++ db/seeds.rb
@@ -0,0 +1,2 @@
+Recipe.create(name: "a", ingredients: "b", cooktime: "c")
+Recipe.create(name: "1", ingredients: "2", cooktime: "3")
